21abde4130dbbb2e2c1c9f2c10fca51906736646
Moose17 <63780114+Mooose17@users.noreply.github.com>
2023-01-09 12:16:45 +0000

    creates some functionality to move pieces

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import { Chessboard } from "react-chessboard";
+import { useState } from "react";
+import { Chess } from "chess.js";
 
 function App() {
+  const [game, setGame] = useState(new Chess());
+
+  function makeAMove(move) {
+    const result = game.move(move);
+    setGame(game);
+    console.log(game, result);
+    return result; // null if the move was illegal, the move object if the move was legal
+  }
+
+  const onDrop = (sourceSquare, targetSquare, piece) => {
+    const move = makeAMove({
+      from: sourceSquare,
+      to: targetSquare,
+      // promotion: "q", // always promote to a queen for example simplicity
+    });
+
+    if (move === null) {
+      return false;
+    }
+    return true;
+  };
+
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Footer />
+      <div className="boardContainer">
+        <Chessboard position={game.fen()} onPieceDrop={onDrop} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer__div">
+      <h1>React Chess Game</h1>
+    </div>
+  );
+};
+
+export default Footer;
